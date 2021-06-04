361ca33ce523980fc9ac3012b1c2a258240ca66c
Brendan Tschuy <brendan.tschuy@cdk.com>
2021-06-04 12:47:27 -0700

    UPDATE: finished project.

--- client.py
+++ client.py
@@ -9,12 +9,44 @@ HEADER = 64
 FORMAT = "utf-8"
 SERVER = "127.0.0.1"
 
-
+# list of ops
 SET_USERNAME = "0001"
 SEND_MESSAGE = "0002"
-JOIN_CHANNEL = "0003"
+JOIN_ROOM = "0003"
 NAME_CHANGE = "0004"
 DISCONNECT = "0005"
+CREATE_ROOM = "0006"
+SEND_TO_ROOM = "0007"
+LEAVE_ROOM = "0008"
+PRIVATE_MESSAGE = "0009"
+LIST_ROOMS = "0010"
+LIST_MEMBERS = "0011"
+LIST_ONLINE = "0012"
+
+SERVER_CRASH = "XXXX"
+
+OPS_MAP = {
+    "!join" : JOIN_ROOM,
+    "!namechange" : NAME_CHANGE,
+    "!create" : CREATE_ROOM,
+    "!leave" : LEAVE_ROOM,
+    "!room" : SEND_TO_ROOM,
+    "!pm" : PRIVATE_MESSAGE
+}
+
+def showHelp():
+    print("========================== HELP ========================\n" \
+        f"{'!namechange <name>': <20} {'change your name to <name>': >35}\n"\
+        f"{'!create <name>': <20} {'create room <name>': >35}\n"\
+        f"{'!join <name>': <20} {'join room <name>': >35}\n"\
+        f"{'!leave <name>': <20} {'leave room <name>': >35}\n"\
+        f"{'!room <name>': <20} {'send chat to room <name>': >35}\n"\
+        f"{'!pm <user> <msg>': <20} {'private message': >35}\n"\
+        f"{'!members <room>': <20} {'list members of room': >35}\n"\
+        f"{'!rooms': <20} {'list all rooms': >35}\n"\
+        f"{'!online': <20} {'list all online users': >35}\n"\
+        f"{'!dc': <20} {'disconnect from server': >35}\n"\
+            )
 
 def send(msg, op):
     message = msg.encode(FORMAT)
@@ -32,20 +64,20 @@ def decodeOp(msg):
         return msg, SEND_MESSAGE
     if(op == "!dc"):
         return msg, DISCONNECT
-    msg = msg.split(' ')[1]
-    print("op = ", op)
-    print("msg = ", msg)
-    if(op == "!namechange"):
-        print("name change")
-        return msg, NAME_CHANGE
-    if(op == "!joinchannel"):
-        return msg, JOIN_CHANNEL
-    
-def prompt():
-    msg_str = input("> ")
-    msg, op = decodeOp(msg_str)
-    send(msg, op)
-
+    if op == "!help":
+        showHelp()
+        return False, False
+    if op == "!rooms":
+        return msg, LIST_ROOMS
+    if op == "!members":
+        return msg, LIST_MEMBERS
+    if op == "!online":
+        return msg, LIST_ONLINE
+    if op in OPS_MAP:
+        msg = " ".join(msg.split(' ')[1:])
+        return msg, OPS_MAP[op]
+    else:
+        raise "OOPS"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.connect(ADDR)
@@ -57,15 +89,19 @@ print("> ", end='')
 
 while True:
     try:
-        # print("> ", end='')
         socketList = [ sys.stdin, server ]
         reads, writes, error = select.select(socketList, [], [])
 
         for skt in reads:
             if skt == sys.stdin:
-                line = sys.stdin.readline().strip()
-                msg, op = decodeOp(line)
-                send(msg, op)
+                try:
+                    line = sys.stdin.readline().strip()
+                    msg, op = decodeOp(line)
+                    if(op):
+                        send(msg, op)
+                except:
+                    print("Unknown op code. Type !help for more information.")
+                    continue
                 if(op == DISCONNECT):
                     print("Disconnecting from server...")
                     exit()
@@ -74,11 +110,11 @@ while True:
                 if(data):
                     rcvd = data.decode(FORMAT).strip()
                     if(rcvd == "XXXX"):
-                        print("Server has disconnected.")
+                        print("Server has disconnected. 😵")
                         exit()
-                    print(f"{rcvd}\n")
+                    print(f"{rcvd}")
                     print("> ", end='')
     except:
-        #mainly handle SIGINTs
+        # mainly handle SIGINTs
         send("", DISCONNECT)
         exit()
--- server.py
+++ server.py
@@ -3,63 +3,189 @@ import threading
 
 PORT = 5432
 SERVER = '127.0.0.1'
-ADDR = (SERVER, PORT)
+FULL_ADDRESS = (SERVER, PORT)
 HEADER = 64
+OP_LENGTH = 4
 FORMAT = "utf-8"
-
-DISCONNECT_MESSAGE = "!dc"
 
 JOIN_SERVER = "0001"
 SEND_MESSAGE = "0002"
-JOIN_CHANNEL = "0003"
+JOIN_ROOM = "0003"
 NAME_CHANGE = "0004"
 DISCONNECT = "0005"
+CREATE_ROOM = "0006"
+SEND_TO_ROOM = "0007"
+LEAVE_ROOM = "0008"
+PRIVATE_MESSAGE = "0009"
+LIST_ROOMS = "0010"
+LIST_MEMBERS = "0011"
+LIST_ONLINE = "0012"
+
 SERVER_CRASH = "XXXX"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(ADDR)
+
+server.bind(FULL_ADDRESS)
 
 clients = []
+rooms = {}
+users = {}
 
 def broadcast(msg):
+    print(msg.decode(FORMAT))
     for client in clients:
         client.send(msg)
 
+def broadcastToRoom(room, msg):
+    fullMsg = f"<{room}>{msg}"
+    print(fullMsg)
+    for client in rooms[room]:
+        client.send(fullMsg.encode(FORMAT))
+    
+
+def createRoom(name, client, user):
+    print(f"[CREATE ROOM] {user} created room {name}.")
+    client.send(f"You created and joined the room {name}.\n".encode(FORMAT))
+    rooms[name] = [client]
+
+def joinRoom(name, client, user):
+    if name not in rooms:
+        client.send(f"Room {name} does not exist.".encode(FORMAT))
+        print(f"Room {name} does not exist.")
+        return False
+    else:
+        if client in rooms[name]:
+            client.send(f"You are already in the room {name}.".encode(FORMAT))
+            return False
+        else:
+            client.send(f"You joined the room {name}.".encode(FORMAT))
+            print(f"{user} joined the room {name}.")
+            rooms[name].append(client)
+            return True
+
+def leaveRoom(activeRooms, room, client):
+    try:
+        rooms[room].remove(client)
+        client.send(f"You left the room {room}".encode(FORMAT))
+        return activeRooms.remove(room)
+    except:
+        client.send(f"You are not in the room {room}.".encode(FORMAT))
+        return activeRooms
+
+def leaveAllRooms(client):
+    for room in rooms:
+        if client in rooms:
+            rooms[room].remove(client)
+
+def sendToRoom(msg, client, user, activeRooms):
+    room = msg.split(" ")[0]
+    msg = " ".join(msg.split(" ")[1:])
+    if room not in activeRooms:
+        client.send(f"You are not in the room {room}".encode(FORMAT))
+    else:
+        broadcastToRoom(room, f"[{user}]: {msg}")
+
+def privateMessage(msg, client, user):
+    target = msg.split(" ")[0]
+    msg = " ".join(msg.split(" ")[1:])
+    print(f"{user} is attempting to private message {target}.")
+    if target not in users:
+        client.send(f"{target} is not online.".encode(FORMAT))
+    else:
+        print(f"{user} private messaged {target}: {msg}")
+        users[target].send(f"[{user}] private messaged you: {msg}".encode(FORMAT))
+
+def listRooms(client):
+    if not rooms:
+        client.send("There are no rooms on this server.".encode(FORMAT))
+    else:
+        client.send(f"All available rooms: ".encode(FORMAT))
+        for room in rooms:
+            client.send(f"{room}\n".encode(FORMAT))
+
+def listOnline(client):
+    client.send(f"Currently online users: ".encode(FORMAT))
+    for user in users:
+        client.send(f"{user}\n".encode(FORMAT))
+
+def listMembers(room, client):
+    room = " ".join(room.split(" ")[1:])
+    if room not in rooms:
+        client.send(f"Room {room} not found.".encode(FORMAT))
+    else:
+        if not rooms[room]:
+            client.send(f"No members in {room}.".encode(FORMAT))
+        else:
+            client.send(f"Members of {room}:\n".encode(FORMAT))
+            for conn in rooms[room]:
+                for user in users:
+                    if(conn == users[user]):
+                        client.send(f"{user}\n".encode(FORMAT))
+
 def handleClient(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected")
+
+    activeRooms = []
     connected = True
+    username = ''
+
     clients.append(conn)
     while(connected):
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
-            op = conn.recv(4).decode(FORMAT)
+            op = conn.recv(OP_LENGTH).decode(FORMAT)
 
             msg = conn.recv(msg_length).decode(FORMAT)
 
-            if(op == JOIN_SERVER):
+            if op == JOIN_SERVER:
                 username = msg
                 broadcast(f"{username} joined the server.".encode(FORMAT))
-                print(f"{username} joined the server.")
+                users[username] = conn
 
-            if(op == SEND_MESSAGE):
+            if op == SEND_MESSAGE:
                 broadcast(f"[{username}]: {msg}".encode(FORMAT))
-                print(f"[{username}]: {msg}")
 
-            if(op == NAME_CHANGE):
+            if op == NAME_CHANGE:
                 broadcast(f"{username} changed their name to {msg}.".encode(FORMAT))
-                print(f"{username} changed their name to {msg}.")
                 username = msg 
 
-            if(op == DISCONNECT):
+            if op == CREATE_ROOM:
+                createRoom(msg, conn, username)
+                activeRooms.append(msg)
+
+            if op == JOIN_ROOM:
+                if(joinRoom(msg, conn, username)):
+                    activeRooms.append(msg)
+
+            if op == SEND_TO_ROOM:
+                sendToRoom(msg, conn, username, activeRooms)
+
+            if op == LEAVE_ROOM:
+                activeRooms = leaveRoom(activeRooms, msg, conn)
+
+            if op == PRIVATE_MESSAGE:
+                privateMessage(msg, conn, username)
+
+            if op == LIST_ROOMS:
+                listRooms(conn)
+
+            if op == LIST_MEMBERS:
+                listMembers(msg, conn)
+
+            if op == LIST_ONLINE:
+                listOnline(conn)
+
+            if op == DISCONNECT:
                 connected = False
                 clients.remove(conn)
+                del users[username]
+                leaveAllRooms(conn)
                 broadcast(f"{username} left the server.".encode(FORMAT))
-                print(f"{username} left the server.")
+                print(f"[DISCONNECT] {username} left the server.")
                 break
-            
-            # conn.send("Msg received".encode(FORMAT))
+
     conn.close()
 
 def start():
@@ -72,8 +198,8 @@ def start():
             thread.start()
             print(f"[NEW CONNECTION] active connections = {threading.activeCount() - 1}")
         except:
-            #mainly handle SIGINTS
-            print("We're crashing")
+            # mainly handle SIGINTS
+            print("\nServer crashed... 😵😵😵")
             broadcast(SERVER_CRASH.encode(FORMAT))
             exit()
 
